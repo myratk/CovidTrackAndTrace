fa4a1e74d48ccf71eb42d63186cb1e7689c197a7
ADI711 <42679100+ADI711@users.noreply.github.com>
2021-04-20 00:23:01 +0100

    Add files via upload

--- LoginPage.php
+++ LoginPage.php
@@ -1,46 +1,18 @@
-<?php
-require 'server.php';
-global $errorsLogin;
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Employee Login</title>
-    <meta name="author" content="Null Pointer">
-    <meta name="description" content="Employee Login for the Covid Trace and Trace System">
-
-    <link rel="stylesheet" href="logindesign.css" />
-</head>
-
-<body>
-<h1>Covid Track and Trace System</h1><br>
-<h2>Employee Login</h2>
-
-<form method="post" action="LoginPage.php">
-    <?php
-    require 'errors.php';
-    displayErrors($errorsLogin);
-    ?>
-    <div>
-        <label for="empID">Employee ID:</label>
-        <input type="text" id="empID" name="empID"><br><br>
+<!DOCTYPE html>    
+<html>    
+<head>    
+    <title>Login Form</title>    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="logindesign.css?v=<?php echo time(); ?>">
+</head>    
+<body>    
+    <div class="main">
+    <p class="sign" align="center">Login</p>
+    <form class="form1">
+      <input class="un " type="text" align="center" placeholder="Username">
+      <input class="pass" type="password" align="center" placeholder="Password"><br><br><br>
+      <a class="submit" align="center">Submit</a>     
     </div>
-    <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br><br>
-    </div>
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br><br>
-    </div>
-    <div>
-        <button type="submit" class="loginBtn" name="login_staff">Login</button>
-    </div>
-</form>
-
-<a href="Homepage.php">Back to Home Page --> </a>
-
-</body>
-</html>
+</div>
+</body>    
+</html>    
